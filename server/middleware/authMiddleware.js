28312c9e52c2e2bf626353aaa7595d378ef9d965
pblnahu1 <pablotorrez90@outlook.com>
2025-07-16 21:33:31 -0300

    fix: delete the comment

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -5,29 +5,25 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const ensureToken = (req, res, next) => {
-    const token = req.header("Authorization")?.split(" ")[1]; // bearer <token> !!! importante
+  const token = req.header("Authorization")?.split(" ")[1]; // bearer <token> !!! importante
 
-    if(!token) {
-        return res.status(401).json({
-            message: "Acceso denegado, token no proporcionado"
-        });
-    }
+  if (!token) {
+    return res.status(401).json({
+      message: "Acceso denegado, token no proporcionado",
+    });
+  }
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET || "my_secret_key");
-        // if(!decoded?.id_u) {
-        //     return res.status(401).json({
-        //         message: "Token inválido"
-        //     });
-        // }
-
-        req.user = decoded;
-        next();
-    } catch (error) {
-        res.status(401).json({
-            message: "Token no válido o expirado"
-        });
-    }
-}
+  try {
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET || "my_secret_key"
+    );
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token no válido o expirado" });
+    next(error);
+  }
+};
 
 export default ensureToken;
