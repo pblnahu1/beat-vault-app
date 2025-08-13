15901d747e90cbd25c6b9c1fed1391d5ba7ea6cb
pblnahu1 <pablotorrez90@outlook.com>
2025-08-13 18:31:45 -0300

    refactor: uso del Loader

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -2,9 +2,13 @@ import { ProductCard } from '../../components/ProductCard/ProductCard';
 import { useProducts } from '../../hooks/useProducts';
 import { AboutUs } from '../../components/AboutUs';
 import { WelcomeUserSection } from '../../components/WelcomeUserSection';
+import { Loader } from '../../components/UI/Loader';
+import { useLoader } from '../../hooks/useLoader';
 
 export const Home = () => {
-  const {products, loading, error} = useProducts();
+  const {products, error} = useProducts();
+  const {loading} = useLoader();
+
   return (
     <>
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
@@ -13,11 +17,7 @@ export const Home = () => {
       
       <h1 className="text-3xl font-bold mb-8 text-slate-50">Catálogo</h1>
 
-      {loading && (
-        <div className="flex justify-center items-center py-12">
-          <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
-        </div>
-      )}
+      {loading && <Loader/>}
 
       {error && (
         <div className="bg-red-300 border border-red-500 text-red-700 px-4 py-3 rounded mb-6">
