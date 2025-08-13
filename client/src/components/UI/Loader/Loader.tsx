b1d83e22ebcbfd2b270a6af10d7aea440a668205
pblnahu1 <pablotorrez90@outlook.com>
2025-08-13 18:25:41 -0300

    feat: loader

---
+++ client/src/components/UI/Loader/Loader.tsx
@@ -0,0 +1,7 @@
+export const Loader = () => {
+    return (
+    <div className="flex justify-center items-center py-12 bg-transparent">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
+    </div>
+    )
+}
